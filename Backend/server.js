94a257d0d70a0f5f74d6608f2113a5d57b0e5257
yashkumar0008 <yashdahiya0008@gmail.com>
2026-04-06 17:10:31 +0530

    update URL

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ connectDB();
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: process.env.CLIENT_URL || 'https://college-cms-frontend-sigma.vercel.app', credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload({ useTempFiles: false, limits: { fileSize: 5 * 1024 * 1024 } }));
