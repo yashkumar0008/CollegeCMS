fad40d5d4295f6fa46a16d724eb6b56ffcee13dd
yashkumar0008 <yashdahiya0008@gmail.com>
2026-04-06 16:52:36 +0530

    update URL

--- Frontend/src/services/api.ts
+++ Frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://localhost:5173/api' });
+const api = axios.create({ baseURL: 'https://college-cms-backend.vercel.app/api' });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
